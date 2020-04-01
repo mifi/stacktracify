cb7a3351dda51c9829e1e96c95e4d5d335172d1b
Michael Peterson <michael_peterson@reyrey.com>
2020-04-01 08:13:46 -0500

    Process all frames on the stack even if some error out.  Added option to stoponfirst error like original code.

--- index.js
+++ index.js
@@ -6,34 +6,39 @@ const fs = require('fs-extra');
 const clipboardy = require('clipboardy');
 const { SourceMapConsumer } = require('source-map');
 
-
 const cli = meow(`
 	Usage
 	  $ stacktracify <map-path>
 
 	Options
     --file, -f  (default is read from clipboard)
+	  --stoponfirst -s (stop processing stack on first error)
 
 	Examples
 	  $ stacktracify /path/to/js.map --file /path/to/my-stacktrace.txt
+	  $ stacktracify /path/to/js.map --file /path/to/my-stacktrace.txt --stoponfirst
 `, {
-	flags: {
-		file: {
-			type: 'string',
-			alias: 'f',
-		}
-	}
+  flags: {
+    file: {
+      type: 'string',
+      alias: 'f',
+    },
+    stoponfirst: {
+      type: 'boolean',
+      alias: 's',
+    },
+  }
 });
 
 
 const file = cli.flags.file;
+const stoponfirst = cli.flags.stoponfirst;
 
 (async () => {
   try {
     const mapPath = cli.input[0];
     if (!mapPath) cli.showHelp();
     const mapContent = JSON.parse(await fs.readFile(mapPath, 'utf-8'));
-    // WTF? promise?
     const smc = await new SourceMapConsumer(mapContent);
 
     let str;
@@ -49,14 +54,24 @@ const file = cli.flags.file;
 
     if (header) console.log(header);
 
+    var errString = '';
     stack.forEach(({ lineNumber, column }) => {
-      const pos = smc.originalPositionFor({ line: lineNumber, column });
-      if (pos && pos.line != null) {
-        console.log(`    at ${pos.name || ''} (${pos.source}:${pos.line}:${pos.column})`);
+      try {
+        const pos = smc.originalPositionFor({ line: lineNumber, column });
+        if (pos && pos.line != null) {
+          console.log(`    at ${pos.name || ''} (${pos.source}:${pos.line}:${pos.column})`);
+        }
+      } catch (err) {
+        if (stoponfirst) {
+          throw (err);
+        }
+        errString += err.stack;
       }
-
-      // console.log('src', smc.sourceContentFor(pos.source));
     });
+
+    if (errString.length > 0) {
+      console.error(errString);
+    }
   } catch (err) {
     console.error(err);
   }
